2164a6308b27dfbb82f2132f8d170e336ad73f37
ilia <iliarushkin@Ilias-MacBook-Air.local>
2018-07-18 20:17:31 -0400

    dev

--- R/standard_dla_sim.R
+++ R/standard_dla_sim.R
@@ -3,7 +3,7 @@
 #' Diffusion-limited aggregation (DLA) cluster can be generated in 2- or 3-dimensional Euclidean space.
 #'
 #' @param N number of particles to be added to the initial cluster (can be Inf)
-#' @param initial a data-frame or matrix of coordinates of the initial cluster. The default is a single seed particle at the origin of 2-dimensional plane.
+#' @param initial a data-frame or matrix of coordinates of the initial cluster. For a single seed particle, a coordinate vector will be also accepted. The default is a single seed particle at the origin of 2-dimensional plane.
 #' The number of columns must be 2 or 3 and serves as the dimensionality of space in which the cluster will be grown.
 #' Column names do not matter, only their order.
 #' @param source a vector of coordinates of the particle source. A single value Inf is understood, but apart from that, must be the same length as the number of columns in \code{initial}.
@@ -15,13 +15,20 @@
 #' It may also print a progress message and export cluster to a file, as controlled by the arguments \code{verbose} and \code{write.to}.
 #' @param verbose logical, indicating whether the function should print out progress statements at the end of each stage controlled by \code{checkin.every}.
 #' @param write.to if not NULL, it is the file path to which the function will export the cluster at the end of each stage controlled by \code{checkin.every}.
-#'
+#' @param phi,theta two vectors of length 2, denoting the boundaries of the sector of particle propagation. \code{phi} is the azimuthal angle (longitude in 3D) and \code{theta} is the polar angle of spherical coordinates in 3D (the pole being 0).
 #' @return A dataframe containing coordinates of cluster particles. The number of rows will be the number of points in the initial cluster plus \code{N}.
 #' @export
 #'
 #' @examples
-dla=function(N=5000, initial=data.frame(x=0,y=0), source=Inf, abandon.factor=10, diameter.tol=0.01, checkin.every=5000, verbose=TRUE, write.to=NULL){
+dla=function(N=5000, initial=data.frame(x=0,y=0), source=Inf, abandon.factor=10, diameter.tol=0.01, checkin.every=5000, verbose=TRUE, write.to=NULL
+             ,phi=c(0,2*pi),theta=c(0,pi)){
   tic=proc.time()[3]
+
+  if(is.null(dim(initial))){
+    initial=t(matrix(initial))
+  }
+  phi=sort(phi)
+  costheta=sort(cos(theta))
 
   d=ncol(initial)
   if(is.null(checkin.every)) checkin.every=Inf
@@ -51,14 +58,14 @@ dla=function(N=5000, initial=data.frame(x=0,y=0), source=Inf, abandon.factor=10,
 
     if(d==2){
       while(nrow(df)<N){
-        df=dla2dinf(N=min(checkin.every,N-nrow(df)),initial=df,abandon.factor = abandon.factor,diameter.tol=diameter.tol)
+        df=dla2dinf(N=min(checkin.every,N-nrow(df)),initial=df,abandon.factor = abandon.factor,diameter.tol=diameter.tol,phi=phi)
         if(export) saveRDS(df,file=write.to)
         if(verbose) cat(paste0('Particles: ',nrow(df),'. Elapsed time: ',round(proc.time()[3]-tic),' sec.\n'))
       }
     }
     if(d==3){
       while(nrow(df)<N){
-        df=dla3dinf(N=min(checkin.every,N-nrow(df)),initial=df,abandon.factor = abandon.factor,diameter.tol=diameter.tol)
+        df=dla3dinf(N=min(checkin.every,N-nrow(df)),initial=df,abandon.factor = abandon.factor,diameter.tol=diameter.tol,phi=phi,costheta=costheta)
         if(export) saveRDS(df,file=write.to)
         if(verbose) cat(paste0('Particles: ',nrow(df),'. Elapsed time: ',round(proc.time()[3]-tic),' sec.\n'))
       }
@@ -70,7 +77,7 @@ dla=function(N=5000, initial=data.frame(x=0,y=0), source=Inf, abandon.factor=10,
 
     if(d==2){
       while(nrow(df)<N){
-        df=dla2dsource(N=min(checkin.every,N-nrow(df)),initial=df, source=source, abandon.factor = abandon.factor,diameter.tol=diameter.tol)
+        df=dla2dsource(N=min(checkin.every,N-nrow(df)),initial=df, source=source, abandon.factor = abandon.factor,diameter.tol=diameter.tol,phi=phi)
         if(all(df[nrow(df),]==source)){
           cat('Cluster blocked the source.\n')
           df=df[!duplicated(df),]
@@ -83,7 +90,7 @@ dla=function(N=5000, initial=data.frame(x=0,y=0), source=Inf, abandon.factor=10,
     }
     if(d==3){
       while(nrow(df)<N){
-        df=dla3dsource(N=min(checkin.every,N-nrow(df)),initial=df, source=source, abandon.factor = abandon.factor,diameter.tol=diameter.tol)
+        df=dla3dsource(N=min(checkin.every,N-nrow(df)),initial=df, source=source, abandon.factor = abandon.factor,diameter.tol=diameter.tol,phi=phi,costheta=costheta)
         if(all(df[nrow(df),]==source)){
           cat('Cluster blocked the source.\n')
           df=df[!duplicated(df),]
@@ -99,9 +106,10 @@ dla=function(N=5000, initial=data.frame(x=0,y=0), source=Inf, abandon.factor=10,
   return(df)
 }
 
-dla2dinf=function(N=200,initial=data.frame(x=0,y=0), abandon.factor=10, diameter.tol=0.01){
+dla2dinf=function(N=200,initial=data.frame(x=0,y=0), abandon.factor=10, diameter.tol=0.01, phi=c(0,2*pi)){
 
-  tau=2*pi
+  phi0=phi[1]
+  phi1=phi[2]
   X=initial[,1]
   Y=initial[,2]
   R=sqrt(max(X^2+Y^2))
@@ -115,7 +123,8 @@ dla2dinf=function(N=200,initial=data.frame(x=0,y=0), abandon.factor=10, diameter
 
       RStart=R+1
       Abandon=RStart*abandon.factor
-      phi=runif(1,0,tau)
+      phi=runif(1,phi0,phi1)+pi
+      # phi=tau*(0.5+rbeta(1,2,2))
       x=RStart*cos(phi)
       y=RStart*sin(phi)
 
@@ -124,7 +133,7 @@ dla2dinf=function(N=200,initial=data.frame(x=0,y=0), abandon.factor=10, diameter
 
     while(minDist>1){
 
-      phi=runif(1,0,tau)
+      phi=runif(1,phi0,phi1)
       Rcur=minDist-tolerated
       x=x+Rcur*cos(phi)
       y=y+Rcur*sin(phi)
@@ -152,9 +161,10 @@ dla2dinf=function(N=200,initial=data.frame(x=0,y=0), abandon.factor=10, diameter
   return(df)
 }
 
-dla2dsource=function(N=200,initial=data.frame(x=0,y=0), source=c(100,0), abandon.factor=10, diameter.tol=0.01){
+dla2dsource=function(N=200,initial=data.frame(x=0,y=0), source=c(100,0), abandon.factor=10, diameter.tol=0.01, phi=c(0,2*pi)){
 
-  tau=2*pi
+  phi0=phi[1]
+  phi1=phi[2]
   X=initial[,1]
   Y=initial[,2]
   R=sqrt(max(X^2+Y^2))
@@ -177,7 +187,7 @@ dla2dsource=function(N=200,initial=data.frame(x=0,y=0), source=c(100,0), abandon
 
     while(minDist>1){
 
-      phi=runif(1,0,tau)
+      phi=runif(1,phi0,phi1)
       Rcur=minDist-tolerated
       x=x+Rcur*cos(phi)
       y=y+Rcur*sin(phi)
@@ -205,90 +215,12 @@ dla2dsource=function(N=200,initial=data.frame(x=0,y=0), source=c(100,0), abandon
   return(df)
 }
 
-dla3d=function(N=200,initial=data.frame(x=0,y=0,z=0), source=Inf, abandon.factor=10, diameter.tol=0.01,write.every=3600, write.to=NULL){
+dla3dinf=function(N=200,initial=data.frame(x=0,y=0,z=0), abandon.factor=10, diameter.tol=0.01, phi=c(0,2*pi),costheta=c(-1,1)){
 
-  tau=2*pi
-  X=initial[,1]
-  Y=initial[,2]
-  Z=initial[,3]
-  R=sqrt(max(X^2+Y^2+Z^2))
-
-  tolerated=1-diameter.tol #tolerated particle diameter
-  Ab=0
-
-  n=0
-
-  from_inf=any(is.infinite(source))
-  Rsource=sqrt(sum(source^2))
-
-  tic=proc.time()[3]
-  while (n<N){
-    if(from_inf){
-      RStart=R+1
-      Abandon=RStart*abandon.factor
-      phi=runif(1,0,tau)
-      cosTheta=runif(1,-1,1)
-      sinTheta=sqrt(1-cosTheta^2)
-      x=RStart*cos(phi)*sinTheta
-      y=RStart*sin(phi)*sinTheta
-      z=RStart*cosTheta
-    }else{
-      Abandon=max(Rsource,R)*abandon.factor
-      x=source[1]
-      y=source[2]
-      z=source[3]
-    }
-
-    minDist=sqrt(min((X-x)^2+(Y-y)^2+(Z-z)^2))
-    Ab=0
-
-    while(minDist>1){
-
-      Rcur=minDist-tolerated
-      phi=runif(1,0,tau)
-      cosTheta=runif(1,-1,1)
-      sinTheta=sqrt(1-cosTheta^2)
-      x=x+Rcur*cos(phi)*sinTheta
-      y=y+Rcur*sin(phi)*sinTheta
-      z=z+Rcur*cosTheta
-
-      minDist=sqrt(min((X-x)^2+(Y-y)^2+(Z-z)^2))
-
-
-      if(minDist>Abandon){
-        Ab=1
-        break
-      }
-    }
-
-    if(Ab==0){
-      n=n+1
-      X=c(X,x)
-      Y=c(Y,y)
-      Z=c(Z,z)
-      R=max(R,sqrt(x^2+y^2+z^2))
-    }
-
-    if(!is.null(write.to)){
-      if(proc.time()[3]-tic > write.every){
-        cat('Particles:',length(X),'\n')
-        saveRDS(data.frame(x=X,y=Y,z=Z),file=write.to)
-        tic=proc.time()[3]
-      }
-    }
-
-  }
-
-  df=data.frame(x=X,y=Y,z=Z)
-  if(!is.null(write.to)){
-    saveRDS(df,file=write.to)
-  }
-  return(df)
-}
-
-dla3dinf=function(N=200,initial=data.frame(x=0,y=0,z=0), abandon.factor=10, diameter.tol=0.01){
-
-  tau=2*pi
+  phi0=phi[1]
+  phi1=phi[2]
+  costheta0=costheta[1]
+  costheta1=costheta[2]
   X=initial[,1]
   Y=initial[,2]
   Z=initial[,3]
@@ -303,8 +235,8 @@ dla3dinf=function(N=200,initial=data.frame(x=0,y=0,z=0), abandon.factor=10, diam
 
       RStart=R+1
       Abandon=RStart*abandon.factor
-      phi=runif(1,0,tau)
-      cosTheta=runif(1,-1,1)
+      phi=runif(1,phi0,phi1)+pi
+      cosTheta=runif(1,-costheta1,-costheta0)
       sinTheta=sqrt(1-cosTheta^2)
       x=RStart*cos(phi)*sinTheta
       y=RStart*sin(phi)*sinTheta
@@ -317,8 +249,8 @@ dla3dinf=function(N=200,initial=data.frame(x=0,y=0,z=0), abandon.factor=10, diam
     while(minDist>1){
 
       Rcur=minDist-tolerated
-      phi=runif(1,0,tau)
-      cosTheta=runif(1,-1,1)
+      phi=runif(1,phi0,phi1)
+      cosTheta=runif(1,costheta0,costheta1)
       sinTheta=sqrt(1-cosTheta^2)
       x=x+Rcur*cos(phi)*sinTheta
       y=y+Rcur*sin(phi)*sinTheta
@@ -348,9 +280,12 @@ dla3dinf=function(N=200,initial=data.frame(x=0,y=0,z=0), abandon.factor=10, diam
   return(df)
 }
 
-dla3dsource=function(N=200,initial=data.frame(x=0,y=0,z=0), source=c(100,0,0), abandon.factor=10, diameter.tol=0.01){
+dla3dsource=function(N=200,initial=data.frame(x=0,y=0,z=0), source=c(100,0,0), abandon.factor=10, diameter.tol=0.01, phi=c(0,2*pi),costheta=c(-1,1)){
 
-  tau=2*pi
+  phi0=phi[1]
+  phi1=phi[2]
+  costheta0=costheta[1]
+  costheta1=costheta[2]
   X=initial[,1]
   Y=initial[,2]
   Z=initial[,3]
@@ -376,8 +311,8 @@ dla3dsource=function(N=200,initial=data.frame(x=0,y=0,z=0), source=c(100,0,0), a
     while(minDist>1){
 
       Rcur=minDist-tolerated
-      phi=runif(1,0,tau)
-      cosTheta=runif(1,-1,1)
+      phi=runif(1,phi0,phi1)
+      cosTheta=runif(1,costheta0,costheta1)
       sinTheta=sqrt(1-cosTheta^2)
       x=x+Rcur*cos(phi)*sinTheta
       y=y+Rcur*sin(phi)*sinTheta
